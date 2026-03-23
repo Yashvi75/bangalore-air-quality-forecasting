3c93c85ee0f905af0394a5945217262ccc546fef
Yashvi Vyas <yashvivyas.work@gmail.com>
2026-03-23 18:04:13 +0530

    Fix print statements and update file path

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,15 +3,15 @@ import pandas as pd
 def load_data(filepath):
     try:
         df = pd.read_csv(filepath)
-        print("✅ Data loaded successfully")
+        print("Data loaded successfully")
         return df
     except Exception as e:
-        print(f"❌ Error loading data: {e}")
+        print(f"Error loading data: {e}")
         return None
 
 
 if __name__ == "__main__":
-    file_path = "/Users/yashvivyas/Documents/bangalore-air-quality-forecasting/data/city_day.csv"
+    file_path = "..data/city_day.csv"
     df = load_data(file_path)
 
     if df is not None:
